a5dd56f715f43e82660015667b1abe0e09fa4df8
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2017-11-25 23:31:14 -0300

    Add root home index to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :queries
-  get 'home/index'
-
+  root 'home#index'
   devise_for :accounts
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :queries
 end
